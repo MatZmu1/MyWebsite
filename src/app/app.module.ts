dfe312e93acccb7f5c9221e5353ee636ee20dcbd
Mateusz Zmuda <mateusz.zmuda3@gmail.com>
2019-05-12 18:13:14 +0200

    betaVersion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,80 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ErrorComponent } from "./error/error.component";
+import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from "./main/main.component";
+import { RouterModule, Routes } from "@angular/router";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FooterComponent } from "./footer/footer.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+} from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MatCardModule } from "@angular/material/card";
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent
+  },
+  {
+    path: "about-me",
+    component: AboutComponent
+  },
+  {
+    path: "portfolio",
+    component: PortfolioComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "404",
+    component: ErrorComponent
+  },
+  {
+    path: "**",
+    redirectTo: "/404"
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ErrorComponent,
+    HeaderComponent,
+    MainComponent,
+    PortfolioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    AngularFontAwesomeModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
