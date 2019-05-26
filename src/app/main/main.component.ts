608e057719d6fe69d3a75c01f600371bb20975b4
Mateusz Zmuda <mateusz.zmuda3@gmail.com>
2019-05-26 20:57:17 +0200

    added type write-effect function to the main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
 })
 export class MainComponent implements OnInit {
+  private typewriter_text: string =
+    "<div> The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. ~Steve Jobs </div>";
+  private typewriter_display: string = "";
 
-  constructor() { }
-
-  ngOnInit() {
+  typingCallback(that) {
+    let total_length = that.typewriter_text.length;
+    let current_length = that.typewriter_display.length;
+    if (current_length < total_length) {
+      that.typewriter_display += that.typewriter_text[current_length];
+    } else {
+      that.typewriter_display = "";
+    }
+    setTimeout(that.typingCallback, 100, that);
   }
-
+  constructor() {}
+  ngOnInit() {
+    this.typingCallback(this);
+  }
 }
